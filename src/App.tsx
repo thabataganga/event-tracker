8f68136f38255313393250c3eaf14bc4b33ebda5
thabataganga <ganga.thabata@gmail.com>
2022-07-10 20:55:25 -0300

    deletar evento

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,21 @@ import { useState } from 'react';
 import style from './App.module.scss';
 import Card from './components/Card';
 import Formulario from './components/Formulario';
-import { IEvento } from './interfaces/IEvento';
 import Calendario from './components/Calendario';
 import ListaDeEventos from './components/ListaDeEventos';
 import { RecoilRoot } from "recoil"
 
 function App() {
 
-  //  const [eventos, setEventos] = useState<IEvento[]>([])
-
-
   const [filtro, setFiltro] = useState<Date | null>()
 
-  const adicionarEvento = (evento: IEvento) => {
-    /*     evento.id = Math.round((new Date()).getTime() / 1000)
-        eventos.push(evento)
-        console.log(eventos);
-        setEventos([...eventos]) */
-  }
+
   const alterarStatusEvento = (id: number) => {
     /*     const evento = eventos.find(evento => evento.id === id)
         if (evento) {
           evento.completo = !evento.completo
         }
         setEventos([...eventos]) */
-  }
-  const deletarEvento = (id: number) => {
-    /*  setEventos([...eventos.filter(evento => evento.id !== id)]) */
   }
 
   const aplicarFiltro = (data: Date | null) => {
@@ -53,7 +41,6 @@ function App() {
             <ListaDeEventos
               aoFiltroAplicado={aplicarFiltro}
               aoAlterarStatus={alterarStatusEvento}
-              aoDeletarEvento={deletarEvento}
             />
           </Card>
         </div>
