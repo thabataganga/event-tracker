6e5ab9f787bf2eae84ff807aa480d2e3d067bdbc
thabataganga <ganga.thabata@gmail.com>
2022-07-10 20:23:06 -0300

    add evento com recoil

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ function App() {
       <div className={style.App}>
         <div className={style.Coluna}>
           <Card>
-            <Formulario aoSalvar={adicionarEvento} />
+            <Formulario />
           </Card>
           <hr />
           <Card>
