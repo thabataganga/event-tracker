dacca6879095cf6e3b4090c090c4993e823cab42
thabataganga <ganga.thabata@gmail.com>
2022-07-10 23:03:06 -0300

    json server

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -4,22 +4,7 @@ import { IFiltroDeEvento } from "../interfaces/IFiltroDeEventos";
 
 export const listaDeEventosState = atom<IEvento[]>({
   key: "listaDeEventosState",
-  default: [
-    {
-      descricao: "Estudar React",
-      inicio: new Date("2022-07-10T09:00"),
-      fim: new Date("2022-07-10T13:00"),
-      completo: false,
-      id: 1642342747,
-    },
-    {
-      descricao: "Estudar Recoil",
-      inicio: new Date("2022-07-09T09:00"),
-      fim: new Date("2022-07-09T11:00"),
-      completo: false,
-      id: 1642342959,
-    },
-  ],
+  default: [],
 });
 
 export const filtroDeEventos = atom<IFiltroDeEvento>({
