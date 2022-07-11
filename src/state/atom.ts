f8db921f8dedd70a0f7744504e9b08a5ee08d6bb
thabataganga <ganga.thabata@gmail.com>
2022-07-10 23:17:43 -0300

    finlizacao do curso

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,10 +1,11 @@
 import { atom } from "recoil";
 import { IEvento } from "../interfaces/IEvento";
 import { IFiltroDeEvento } from "../interfaces/IFiltroDeEventos";
+import { eventosAsync } from "./seletores";
 
 export const listaDeEventosState = atom<IEvento[]>({
   key: "listaDeEventosState",
-  default: [],
+  default: eventosAsync,
 });
 
 export const filtroDeEventos = atom<IFiltroDeEvento>({
--- src/state/seletores/index.ts
+++ src/state/seletores/index.ts
@@ -1,4 +1,5 @@
 import { selector } from "recoil";
+import { IEvento } from "../../interfaces/IEvento";
 import { filtroDeEventos, listaDeEventosState } from "../atom";
 
 export const eventosFiltradosState = selector({
@@ -16,5 +17,18 @@ export const eventosFiltradosState = selector({
       return ehOMesmoDia;
     });
     return eventos;
+  },
+});
+
+export const eventosAsync = selector({
+  key: "eventosAsync",
+  get: async () => {
+    const respostaHttp = await fetch("http://localhost:8080/eventos");
+    const eventosJson: IEvento[] = await respostaHttp.json();
+    return eventosJson.map((evento) => ({
+      ...evento,
+      inicio: new Date(evento.inicio),
+      fim: new Date(evento.fim),
+    }));
   },
 });
